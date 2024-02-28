a35e6320d41af87ac4bcc20f3450a0cecfe06f6b
zackzonexx <zackzonexx@gmail.com>
2024-02-28 09:59:13 +0700

    feat: adjust readme and setup

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-
-long_description = ""
-# Get the long description from the README file
-with open("README.rst", "r") as f:
-    long_description = f.read()
 
 setup(
     name="datadog-muted-alert-checker",
-    version="1.0.1",
+    version="1.0.2",
     license="MIT",
-    long_description=long_description,
-    url="https://github.com/zackzonexx/datadog-muted-alert-checker.git",
+    url="https://github.com/zackzonexx/datadog-muted-alert-checker",
     author="Zackzonexx",
+    long_description=open("README.rst").read(),
+    long_description_content_type="text/x-rst",
     author_email="zackzonexx@gmail.com",
-    packages=["package"],
+    packages=find_packages(),
     keywords="Datadog Muted Alert Checker",
     classifiers=[
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",  # Specify your license type
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
         "Natural Language :: English",
     ],
     install_requires=[
@@ -31,4 +31,9 @@ setup(
         "google-api-python-client",
         "pytest",
     ],
+    entry_points={
+        "console_scripts": [
+            "datadog-checker=package.main:main",
+        ],
+    },
 )
