843eae62e4838a00675af694b4d351e6c86d9e09
alfatih-shell <riyanpratomo@gmail.com>
2024-03-11 13:13:50 +0700

    feat: adjust unit test for simple case

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,57 +1,29 @@
-import unittest
-from unittest.mock import patch, MagicMock
-from package.main import main
+from unittest.mock import patch
+from package.main import get_arg_value
 
 
-class TestMain(unittest.TestCase):
-    @patch(
-        "sys.argv",
-        [
-            "main.py",
-            "--datadog-api-key",
-            "dummy_api_key",
-            "--datadog-app-key",
-            "dummy_app_key",
-            "--team-name",
-            "dummy_team",
-            "--opsgenie-api-key",
-            "dummy_opsgenie_key",
-        ],
-    )
-    @patch("package.main.get_muted_alerts")
-    @patch("package.main.create_alert_payload")
-    @patch("package.opsgenie_utils.opsgenie_sdk")
-    def test_main_function(
-        self, mock_sdk, mock_create_alert_payload, mock_get_muted_alerts
-    ):
-        # Mocking return values
-        mock_get_muted_alerts.return_value = {
-            "alert1": ["123", "456"],
-            "alert2": ["789"],
-        }
+def test_get_arg_value():
+    # Define input arguments
+    args = [
+        "--datadog-api-key",
+        "dummy_api_key",
+        "--datadog-app-key",
+        "dummy_app_key",
+        "--team-name",
+        "dummy_team",
+        "--opsgenie-api-key",
+        "dummy_opsgenie_key",
+    ]
 
-        # Mocking the alert payload creation
-        mock_create_alert_payload.return_value = MagicMock()
+    # Call the function with the mocked input arguments
+    with patch("sys.argv", args):
+        datadog_api_key = get_arg_value("--datadog-api-key")
+        datadog_app_key = get_arg_value("--datadog-app-key")
+        team_name = get_arg_value("--team-name")
+        opsgenie_api_key = get_arg_value("--opsgenie-api-key")
 
-        # Mocking the Opsgenie API client and its create_alert method
-        mock_client = MagicMock()
-        mock_sdk.AlertApi.return_value = mock_client
-
-        # Call the main function
-        main()
-
-        # Check if get_muted_alerts was called with correct arguments
-        mock_get_muted_alerts.assert_called_once_with(
-            "dummy_api_key", "dummy_app_key", "dummy_team"
-        )
-
-        # Check if create_alert_payload was called with correct arguments
-        mock_create_alert_payload.assert_any_call("alert1", ["123", "456"])
-        mock_create_alert_payload.assert_any_call("alert2", ["789"])
-
-        # Check if create_alert method of the Opsgenie API client was called
-        mock_client.create_alert.assert_called_once()
-
-
-if __name__ == "__main__":
-    unittest.main()
+    # Assert the results
+    assert datadog_api_key == "dummy_api_key"
+    assert datadog_app_key == "dummy_app_key"
+    assert team_name == "dummy_team"
+    assert opsgenie_api_key == "dummy_opsgenie_key"
