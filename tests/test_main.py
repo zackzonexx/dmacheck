eb1db86557ee356132069fc6e70c5892838fbcbb
alfatih-shell <riyanpratomo@gmail.com>
2024-03-11 13:27:06 +0700

    feat: adjust fix test

--- tests/test_main.py
+++ tests/test_main.py
@@ -33,9 +33,13 @@ def test_main_with_args(
     expected_datadog_app_key,
     expected_opsgenie_api_key,
 ):
-    # Call the main function with the mocked command line arguments
-    with patch("sys.argv", args):
-        main()
+    # Mock the functions or classes called within main() to avoid executing the entire logic
+    with patch("package.main.get_muted_alerts"):
+        with patch("package.main.create_alert_payload"):
+            with patch("package.opsgenie_utils.opsgenie_sdk.AlertApi.create_alert"):
+                # Call the main function with the mocked command line arguments
+                with patch("sys.argv", args):
+                    main()
 
     # Access expected_team_name, expected_datadog_api_key, expected_datadog_app_key, and expected_opsgenie_api_key within the test function
     assert expected_team_name == "dummy_team"
